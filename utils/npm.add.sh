9e5bc6c96f20983a08619a336258bd4900454ddf
dadduedo <dadduedo@gmail.com>
2023-09-26 10:46:30 +0200

    add utils folder,fix package.json

---
+++ utils/npm.add.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ABSOLUTE_PROJECT_PATH=$(git rev-parse --show-toplevel)
+
+docker run -ti \
+    -v ${ABSOLUTE_PROJECT_PATH}:/projects/stats-generator \
+    -w /projects/stats-generator \
+    node:16 npm i $@
